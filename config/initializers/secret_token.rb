7dcbfa727941fd7597644c03f091e91316acf550
Arkadiusz Turlewicz <quirek@gmail.com>
2012-09-08 11:40:44 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberWithCoffee::Application.config.secret_token = '52ca65238294c7fd34029f28f5c84eb9b74f9bfe6196fc5fb51212506040ec5bc915aa3a6842a91466d23c3c92a782d56f27b6db00c7da50179417bb57b4a438'
