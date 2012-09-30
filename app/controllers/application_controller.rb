c4ab3b5d124c32806bff1a392de45e2c8cec1690
Arkadiusz Turlewicz <quirek@gmail.com>
2012-09-30 22:10:06 +0900

    Add basic auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  if Rails.env.production?
+    http_basic_authenticate_with :name => ENV['APP_USER'] || 'afs234aa', :password =>  ENV['APP_PASSWORD'] || '234fsd234rq2da'
+    force_ssl
+  end 
 end
